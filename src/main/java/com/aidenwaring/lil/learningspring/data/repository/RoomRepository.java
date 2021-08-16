e23ec917f3deae2a0da4f034cd7dabafcef950a1
awaring <aiden.waring@corelogic.com.au>
2021-08-16 15:45:01 +1000

    Updates to Repository files, created DTO RoomReservation and service ReservationService.

--- src/main/java/com/aidenwaring/lil/learningspring/data/repository/RoomRepository.java
+++ src/main/java/com/aidenwaring/lil/learningspring/data/repository/RoomRepository.java
@@ -1,9 +1,12 @@
 package com.aidenwaring.lil.learningspring.data.repository;
 
+import com.aidenwaring.lil.learningspring.data.entity.Reservation;
 import com.aidenwaring.lil.learningspring.data.entity.Room;
+import java.sql.Date;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface RoomRepository extends CrudRepository<Room, Long> {
+    Iterable<Reservation> findReservationByReservationDate(Date date);
 }
